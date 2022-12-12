8458b541b7968af85745b30b9a12a77c4f0f04c5
Jerance <michael.jerance@gmail.com>
2022-12-12 21:09:01 +0100

    #1 (feat) Add in architecture of project the assets folder with img / theme / font

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:music_app/theme/colors.dart';
+import 'package:music_app/assets/theme/colors.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
